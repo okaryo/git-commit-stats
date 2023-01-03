15283ed8bfbdae29345258465a128f507752fb4e
okaryo <okaryotr@gmail.com>
2023-01-03 21:36:02 +0900

    :sparkles: output commit stats grouped by label

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,37 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"sort"
 
+	"github.com/okaryo/git-commit-stats/lib/commit"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "git commit-stats",
-	Short: "A brief description of your application",
-	Long:  "A brief description of your application",
+	Short: "git commit-stats output a report of your commits",
+	Long:  "git commit-stats output a report of your commits",
 	Run: func(cmd *cobra.Command, args []string) {
+		groupedCommits := commit.GroupCommitByLabel()
+
+		keys := make([]string, 0, len(groupedCommits))
+		for key := range groupedCommits {
+			keys = append(keys, key)
+		}
+		sort.SliceStable(keys, func(i, j int) bool {
+			return groupedCommits[keys[i]] > groupedCommits[keys[j]]
+		})
+
+		for _, key := range keys {
+			label := key
+			if label == "" {
+				label = "other"
+			}
+			stats := fmt.Sprintf("%s %d", label, groupedCommits[key])
+			fmt.Println(stats)
+		}
 	},
 }
 
